9ce66b88bf3da6bb7cf1c313ef321d9608441308
Junpei-Nakasone <nakasone.junpei0719@gmail.com>
2019-09-12 22:19:01 +0900

    add getCats method

--- src/main/main.go
+++ src/main/main.go
@@ -7,6 +7,28 @@ import (
 	"github.com/labstack/echo"
 )
 
+func getCats(c echo.Context) error {
+	catName := c.QueryParam("name")
+	catType := c.QueryParam("type")
+
+	dataType := c.Param("data")
+
+	if dataType == "string" {
+		return c.String(http.StatusOK, fmt.Sprintf("Your cat name is %s\nand  type is %s", catName, catType))
+	}
+
+	if dataType == "json" {
+		return c.JSON(http.StatusOK, map[string]string{
+			"name": catName,
+			"type": catType,
+		})
+	}
+	return c.JSON(http.StatusBadRequest, map[string]string{
+		"error": "you need to specify your cat's name and type.",
+	})
+
+}
+
 func main() {
 	fmt.Println("WELCOME To SERVER")
 
@@ -15,6 +37,7 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "HELLO FROM WEBSite")
 	})
+	e.GET("/cats/:data", getCats)
 
 	e.Start(":8000")
 }
